43b4a48ce8ef20481c50030f6ad4118ad859397b
trungqcao <trungcao241998@gmail.com>
2018-12-11 00:15:52 +1000

    fixed directory path

--- Firmware/LoRaRxNode/Debug/Src/subdir.mk
+++ Firmware/LoRaRxNode/Debug/Src/subdir.mk
@@ -30,7 +30,7 @@ Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F446xx -I"D:/Research/Firmware/LoRaRxNode/Inc" -I"D:/Research/Firmware/LoRaRxNode/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Research/Firmware/LoRaRxNode/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Research/Firmware/LoRaRxNode/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Research/Firmware/LoRaRxNode/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F446xx -I"D:/Research/SX1272-LoRa/Firmware/LoRaRxNode/Inc" -I"D:/Research/SX1272-LoRa/Firmware/LoRaRxNode/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Research/SX1272-LoRa/Firmware/LoRaRxNode/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Research/SX1272-LoRa/Firmware/LoRaRxNode/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Research/SX1272-LoRa/Firmware/LoRaRxNode/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
